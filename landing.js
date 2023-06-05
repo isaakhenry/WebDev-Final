0a9fc705536034ed3dc2396ef99f0ad5b1808f47
ithenry <55816847+ithenry@users.noreply.github.com>
2023-06-05 15:33:22 -0700

    Background Image, Accordion Formatting

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ let aboutSection = document.getElementById("About");
 
 const makeImg = () => {
   let img = new Image(400, 400);
-  img.src = "../Images/Isaak.JPEG";
+  img.src = "./Images/Isaak.JPEG";
   img.alt = "Photo of man in a hat.";
   aboutSection.appendChild(img);
 };
@@ -13,6 +13,8 @@ const addBio = () => {
     "Hello! My name is Isaak, and I'm a fresh Computer Science graduate from Portland State University. I've worked as a System Administrator for the last three years, but I'm trying to break into more of an engineering role. Currently, the languages that I practice are C++, Java, Python, JavaScript, TypeScript, and C. The fields within Computer Science which I am excited to explore more are Machine Learning, Embedded Systems, and DevOps. I enjoy outside contact and communication, so feel free to reach out in the contact section.";
   aboutSection.appendChild(p);
 };
+makeImg();
+addBio();
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -28,5 +30,3 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-makeImg();
-addBio();
